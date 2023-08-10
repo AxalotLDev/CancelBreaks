a2bfd02eee097a3fb63c8548c7bbd131b36457b3
AxalotLS <ladyginnic815@gmail.com>
2023-08-10 15:18:19 +0500

    Update to 1.20.1

--- src/main/java/com/axalotl/cancelbreaks/CancelBreaks.java
+++ src/main/java/com/axalotl/cancelbreaks/CancelBreaks.java
@@ -2,11 +2,18 @@ package com.axalotl.cancelbreaks;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.block.BuddingAmethystBlock;
+import net.minecraft.util.ActionResult;
 
 public class CancelBreaks implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        AttackBlockCallback.EVENT.register(new CancelBreakBuddingAmethyst());
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+            if(world != null && world.getBlockState(pos).getBlock() instanceof BuddingAmethystBlock && !player.isCreative()){
+                return ActionResult.FAIL;
+            }
+            return ActionResult.PASS;
+        });
     }
 }
