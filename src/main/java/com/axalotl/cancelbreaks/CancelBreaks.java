864d86d31f9524b49a1b3a750b44351bb8bceb1a
AxalotLS <ladyginnikita500@gmail.com>
2024-06-08 10:17:21 +0500

    - Update to 1.20.6 - Added new blocks: Suspicious Sand, Suspicious Gravel

--- src/main/java/com/axalotl/cancelbreaks/CancelBreaks.java
+++ src/main/java/com/axalotl/cancelbreaks/CancelBreaks.java
@@ -2,6 +2,7 @@ package com.axalotl.cancelbreaks;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.BuddingAmethystBlock;
 import net.minecraft.util.ActionResult;
 
@@ -10,7 +11,10 @@ public class CancelBreaks implements ModInitializer {
     @Override
     public void onInitialize() {
         AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
-            if(world != null && world.getBlockState(pos).getBlock() instanceof BuddingAmethystBlock && !player.isCreative()){
+            if (
+                    world != null &&
+                            (world.getBlockState(pos).getBlock() instanceof BuddingAmethystBlock || world.getBlockState(pos).getBlock().equals(Blocks.SUSPICIOUS_GRAVEL) || world.getBlockState(pos).getBlock().equals(Blocks.SUSPICIOUS_SAND)) &&
+                            !player.isCreative()) {
                 return ActionResult.FAIL;
             }
             return ActionResult.PASS;
